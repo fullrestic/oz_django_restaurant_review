5861ef130f624684666efa31ac966e44abd00793
Ubuntu <ubuntu@ip-172-31-32-141.ap-northeast-2.compute.internal>
2025-08-13 09:24:05 +0000

    EC2 배포

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,7 +4,11 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['PythonAnywhere 배포도메인']
+ALLOWED_HOSTS = ['3.39.237.55', 'ec2-3-39-237-55.ap-northeast-2.compute.amazonaws.com']
+
+INSTALLED_APPS += [
+    'storages',
+]
 
 # Static
 STATIC_URL = 'static/'
@@ -18,7 +22,49 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # DB
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': SECRET['DB']['NAME'],               # 데이터베이스 이름
+        'USER': SECRET['DB']['USER'],               # 사용자 이름
+        'PASSWORD': SECRET['DB']['PASSWORD'],       # 비밀번호
+        'HOST': SECRET['DB']['HOST'],               # 데이터베이스 서버 주소
+        'PORT': SECRET['DB']['PORT'],               # MySQL의 기본 포트
     }
 }
+
+# S3 Storage
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "access_key": SECRET["S3"]["ACCESS"],
+            "secret_key": SECRET["S3"]["SECRET"],
+            "bucket_name": SECRET["S3"]["NAME"],
+            "region_name": SECRET["S3"]["REGION"],
+            "location": "media",
+            "default_acl": "public-read",
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "access_key": SECRET["S3"]["ACCESS"],
+            "secret_key": SECRET["S3"]["SECRET"],
+            "bucket_name": SECRET["S3"]["NAME"],
+            "region_name": SECRET["S3"]["REGION"],
+            "custom_domain": f'{SECRET["S3"]["NAME"]}.s3.amazonaws.com',
+            "location": "static",
+            "default_acl": "public-read",
+        },
+    },
+}
+
+# Static, Media URL
+STATIC_URL = f'https://{SECRET["S3"]["NAME"]}.s3.amazonaws.com/static/'
+MEDIA_URL = f'https://{SECRET["S3"]["NAME"]}.s3.amazonaws.com/media/'
+
+ROOT_URLCONF = 'config.urls'
+
+
+
+
+
